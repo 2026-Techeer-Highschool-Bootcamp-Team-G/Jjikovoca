f2c819218626ba99bca46502d3bf2a502324af1f
0901shc <0901shc@naver.com>
2026-07-23 19:38:07 +0900

    refactor: types.ts StudySubject 타입 제거(학습 과목 선택 폐기)

--- web/src/features/study-setup/model/types.ts
+++ web/src/features/study-setup/model/types.ts
@@ -1,4 +1,3 @@
-// F-28 학습 진입 & 유형 선택 (v2.2). 방식 2택 + 과목(영어/수학) + 영어 유형(수학은 사고과정 고정)
+// F-28 학습 진입 & 유형 선택. 방식 2택(오늘 복습/직접 선택) + 복습 유형(플래시카드/빈칸)
 export type StudyMethod = 'TODAY' | 'PICK'
-export type StudySubject = 'ENGLISH' | 'MATH'
 export type ReviewType = 'FLASHCARD' | 'CLOZE'
